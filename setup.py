2f0292f11f4fe7bbbcb6b64d4a4bcc02d2ff88f4
Loaye <jr.carstens00@gmail.com>
2017-10-24 00:12:10 -0700

    sets scaffold

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mailroom",
+    description="some issues in the mail room",
+    version=0.1,
+    author="Jacob Carstens",
+    license="MIT",
+    py_modules=['mailroom'],
+    package_dir={'': 'src'}
+)
